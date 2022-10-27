cc649a505ab842ff1e46902ea17afa7af6f1842b
Simone Anile <23080613+simusr2@users.noreply.github.com>
2022-10-27 19:43:15 +0200

    Add ASP.NET Core template.

---
+++ AspNetCorePwa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCorePwa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
